dc3d7aaf0eff9fc09a0ccb6522d15b76f369e011
Sadab <127580924+sadabx@users.noreply.github.com>
2025-03-17 14:45:55 +0600

    Create Hello World.cs

---
+++ C#/Hello World.cs
@@ -0,0 +1,21 @@
+using System;
+/*
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+namespace Hello
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write("Hello Solar System");
+            //write does not end the line!
+            Console.WriteLine("Hello Galaxy");
+            // Console.ReadLine();
+        }
+    }
+}
