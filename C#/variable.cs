2fc6970a47a32e1b877d8a9a8637d5d14dcc8253
Sadab <127580924+sadabx@users.noreply.github.com>
2025-03-17 15:43:12 +0600

    Create variable.cs

---
+++ C#/variable.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace variable
+{
+    class program
+    {
+        void Main()
+        {
+            int a = 2;
+            float b = 3.4F;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
